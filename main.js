4b6ab3f47ea043b6039d6021fa3cecb10e831229
Werner Naujokat Tavares <114090325+wntpk3@users.noreply.github.com>
2024-03-01 12:27:20 -0300

    drd

---
+++ main.js
@@ -0,0 +1,13 @@
+const botoes=document.query.SelectorAll(".botao");
+
+for(let i=0;<botoes.length;i++){
+botoes [i].onclick=function(){
+
+for(let i=0;;<botoes.length;j++){
+    
+
+ botoes[i].classList.add("ativo");
+}
+console.log();
+
+}
