dd37152b8bb03b7da55bc5d324d5c9da2b59ed49
Werner Naujokat Tavares <114090325+wntpk3@users.noreply.github.com>
2024-03-08 10:27:42 -0300

    setwet

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 const botoes=document.query.SelectorAll(".botao");
 
-for(let i=0;<botoes.length;i++){
-botoes [i].onclick=function(){
-
 for(let i=0;;<botoes.length;j++){
-    
+    console.log(i);
+
+    botoes [i].onclick=function(){
 
  botoes[i].classList.add("ativo");
 }
-console.log();
-
 }
