4296fdfdcc4430be5041bb62e07817a96bd89b6a
Werner Naujokat Tavares <114090325+wntpk3@users.noreply.github.com>
2024-03-21 10:46:20 -0300

    wwqqw

--- main.js
+++ main.js
@@ -6,9 +6,17 @@ for(let i=0;i <botoes.length;i++){
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
+
+const contaore = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+
+let tempoAtual = new Date()
+
+contaodres[0].textContent = tempoObjetivo1 - tempoAtual;
