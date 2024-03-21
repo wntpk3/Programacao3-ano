1d985275b02a4d7ffd6ca6863c11b4fe5098625a
Werner Naujokat Tavares <114090325+wntpk3@users.noreply.github.com>
2024-03-21 12:19:15 -0300

    alterações quinta feira

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-        for(let j=0;j<botoes.length;j++){
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -14,9 +14,23 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 
-const contaore = document.querySelectorAll(".contador");
+const contador = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 
-let tempoAtual = new Date()
+contaodres[0].textContent = calculaTempo(tempoObjetivo1);
 
-contaodres[0].textContent = tempoObjetivo1 - tempoAtual;
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date()
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(horas / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+}
