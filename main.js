81c502b5c364d068a516494795489c61a747cd65
Werner Naujokat Tavares <114090325+wntpk3@users.noreply.github.com>
2024-04-18 12:20:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
@@ -35,19 +35,18 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0){
-        return [dias,horas,min,seg];
+        return [dias,horas,minutos,segundos];
     } else {
         return [0,0,0,0];
     }
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent.calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent.calculaTempo(tempos[0])[1];
-    document.getElementById("min0").textContent.calculaTempo(tempos[0])[2];
-    document.getElementById("seg0").textContent.calculaTempo(tempos[0])[3];      
     for (let i=0; i<contadores.length;i++){
-        //contadores[i].textContent = calculaTempo(tempos[i]);   
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
     }
 }
 
